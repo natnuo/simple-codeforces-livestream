131192b0f4e04e82480de0eb92b2a5d1e786d3a3
natnuo <natnu212@gmail.com>
2024-10-06 08:22:44 -0400

    remove dist

--- dist/secrets.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SECRETS = void 0;
-exports.SECRETS = {
-    CF_API_KEY: process.env.CODEFORCES_API_KEY,
-    CF_API_SECRET: process.env.CODEFORCES_API_SECRET,
-};
