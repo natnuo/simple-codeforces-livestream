0ef80517182094320341c755c28b3d4c6dc1f4da
natnuo <natnu212@gmail.com>
2024-10-24 16:46:05 -0400

    zip command

--- src/settings.ts
+++ src/settings.ts
@@ -1,19 +1,19 @@
 export let SETTINGS = {
-  "CONTEST_ID": 2022,
+  "CONTEST_ID": 2025,
   "PROBLEM_COLORS": {
     "A": "#ff8800",
     "B": "#ff0088",
     "C": "#88ff00",
-    "D1": "#00ff88",
-    "D2": "#00ff88",
-    "E1": "#8800ff",
-    "E2": "#8800ff"
+    "D": "#00ff88",
+    "E": "#00ff88",
+    "F": "#8800ff",
+    "G": "#8800ff"
   },
   "ACCEPTED_COLOR": "#198754",
   "TESTING_COLOR": "#cfcd30",
   "REJECTED_COLOR": "#dc3545",
   "FROZEN_COLOR": "#1D1987",
-  "FREEZEAT_SECONDS": -1,
+  "FREEZEAT_SECONDS": 5400,
   "MAX_SUBMISSIONS_DISPLAYED": 17,
   "MAX_STANDINGS_DISPLAYED": 10,
   "QUEUE_RELOAD_INTERVAL_MS": 3000,
