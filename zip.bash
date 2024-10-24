1ae690d23d080116ea7b0ab869844ab87fd0d8ef
natnuo <natnu212@gmail.com>
2024-10-24 16:46:56 -0400

    better zip stuff

--- zip.bash
+++ zip.bash
@@ -1,2 +1,2 @@
 sudo apt-get install zip
-zip -r sicoli-v1.0.0.zip . -x dist/**\* node_modules/**\* .gitignore zip.bash
+zip -r zips/sicoli-v1.0.0.zip . -x dist/**\* node_modules/**\* .gitignore zip.bash
