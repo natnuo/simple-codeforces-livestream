02db145f2363e0e1eb22a074ca17c208d032f996
natnuo <natnu212@gmail.com>
2025-04-08 20:10:31 -0400

    negate pen

--- zip.bash
+++ zip.bash
@@ -1,2 +1,2 @@
 sudo apt-get install zip
-zip -r zips/TLE-live-v1.0.0.zip . -x dist/**\* node_modules/**\* .gitignore zip.bash
+zip -r zips/TLE-live-v1.1.2.zip . -x dist/**\* node_modules/**\* zips/**\* .gitignore zip.bash
