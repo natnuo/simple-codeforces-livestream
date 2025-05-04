8c6bf428a21c0687c1dca82f128d5a2c03e24059
natnuo <natnu212@gmail.com>
2025-05-04 19:02:30 -0400

    extra bash scripts, slight log and standings visual tweaks, new example settings.ts

--- zip.bash
+++ zip.bash
@@ -1,2 +1,2 @@
 sudo apt-get install zip
-zip -r zips/TLE-live-v1.1.2.zip . -x dist/**\* node_modules/**\* zips/**\* .gitignore zip.bash
+zip -r zips/TLE-live-v1.1.3a.zip . -x dist/**\* node_modules/**\* zips/**\* .gitignore zip.bash
